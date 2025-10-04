219df9c23f43bc26c21be3c84f7246a9d33f4051
A-rNaV <148805601+A-rNaV@users.noreply.github.com>
2025-10-04 12:04:14 +0530

    Comment out Projects component import
    Comment out the Projects component import in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Navbar } from "./components/Nav/Navbar";
 import {Hero} from "./components/hero/hero.jsx";
 import {About} from "./components/about/about.jsx";
 import {Skills} from "./components/skills/skills.jsx";
-import {Projects} from "./components/projects/projects.jsx";
+// import {Projects} from "./components/projects/projects.jsx";
 import {Contact} from "./components/contact/contact.jsx";
 
 function App() {
@@ -13,7 +13,6 @@ function App() {
     <Hero />
     <About />
     <Skills />
-    <Projects />
     <Contact />
   </div>)
 }
